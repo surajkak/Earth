f45ee68f7a3614503366a4d73d3492b144b7671d
surajkak <surajkak@surajkak.cybage.com>
2019-06-10 19:36:39 +0530

    Add subtraction

--- Beta/src/main/java/Subtract.java
+++ Beta/src/main/java/Subtract.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+ 
+class subtract
+{
+   private static Scanner in;
 
-public class Subtract {
-
+public static void main(String args[])
+   {
+      int x, y, z;
+ 
+      System.out.println("Enter two integers to calculate");
+      in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x - y;
+     
+      System.out.println("Answer of the integers = " + z);}
 }
+   
