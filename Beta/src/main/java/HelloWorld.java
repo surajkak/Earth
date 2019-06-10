e048d2ab8e7ddf2330748cd8676b2fad0217d1c5
surajkak <surajkak@surajkak.cybage.com>
2019-06-10 15:10:28 +0530

    Branch01

--- Beta/src/main/java/HelloWorld.java
+++ Beta/src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 
 	public static void main(String args[]){
 		// TODO Auto-generated method stub
-		System.out.println("Hello,Pune");
+		System.out.println("Hello,World");
 	}
 }
