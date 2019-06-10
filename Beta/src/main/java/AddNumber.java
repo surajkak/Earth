690989c04a419e31c67fa7c2acc17f02b06de27b
surajkak <surajkak@surajkak.cybage.com>
2019-06-10 19:16:08 +0530

    Add two numbers

--- Beta/src/main/java/AddNumber.java
+++ Beta/src/main/java/AddNumber.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+ 
+class AddNumber
+{
+   private static Scanner in;
 
-public class AddNumber {
-
+public static void main(String args[])
+   {
+      int x, y, z;
+ 
+      System.out.println("Enter two integers to calculate their sum");
+      in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+     
+      System.out.println("Sum of the integers = " + z);
+   }
 }
