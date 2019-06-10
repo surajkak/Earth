3fdaf5e4bdd63a9c5f218848243be01d4558f47c
surajkak <surajkak@surajkak.cybage.com>
2019-06-10 17:36:08 +0530

    update name

--- Beta/src/main/java/HelloWorld.java
+++ Beta/src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 
 	public static void main(String args[]){
 		// TODO Auto-generated method stub
-		System.out.println("Hello,Java");
+		System.out.println("Hello,World");
 	}
 }
